ef2c9e90d19392c3a5de79812a2da71e34e39a8d
Shane Scott <shaneradio@gmail.com>
2021-03-24 16:15:01 -0400

    through Nash-flask 4

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,8 +1,10 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/admin/dashboard")
 def adminDashboard():
-    return "<h1>admin dashboard</h1>"
+    return render_template("admin/dashboard.html")
 
 @app.route("/admin/profile")
 def adminProfile():

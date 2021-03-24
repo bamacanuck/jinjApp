607b3b71e16e4d41a123ef12a6d59f4988940eaa
Shane Scott <shaneradio@gmail.com>
2021-03-24 12:54:55 -0400

    ...

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from app import app
+
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/jinja")
+def jinja():
+    return render_template("/jinja.html")
+
+@app.route("/x")
+def redX():
+    return "<h1 style='color : red'>X</h1>"

129021318eeccafb9bb3971eadd295cad19f5be9
Shane Scott <shaneradio@gmail.com>
2021-03-25 16:06:50 -0400

    added a bunch of data for use in jinja examples

--- app/views.py
+++ app/views.py
@@ -11,7 +11,46 @@ def jinja():
 
     my_name = "SRS"
 
-    return render_template("public/jinja.html", my_name=my_name)
+    age = 42
+
+    langs = ["Python", "JavaScript", "Bash", "C", "Ruby"]
+
+    friends = {
+        "Tom": 30,
+        "Amy": 60,
+        "Tony": 56,
+        "Clarissa": 23
+    }
+
+    colors = ("red", "green")
+
+    cool = True
+
+    class GitRemote:
+        def __init__(self, name, desc, url):
+            self.name = name
+            self.desc = desc
+            self.url = url
+
+        def pull(self):
+            return f"pulling repo {self.name}"
+
+        def clone(self):
+            return f"cloning into {self.url}"
+
+    my_remote = GitRemote (
+        name="Flask Jinja",
+        desc="template design tutorial",
+        url="https://github.com/bamacanuck/jinjApp.git"
+    )
+
+    def repeat(x, qty):
+        return x * qty
+
+    
+    return render_template("public/jinja.html", my_name=my_name, age=age,
+    langs=langs, friends=friends, colors=colors, cool=cool,
+    GitRemote=GitRemote, my_remote=my_remote, repeat=repeat)
 
 @app.route("/x")
 def redX():
